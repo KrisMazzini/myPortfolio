d50d1165a42e3e1bc761b812a5b41804c171bb61
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-26 22:31:14 -0300

    adjust body

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import './styles.css'
+import { Container } from './styles'
 
 import { Profile } from '../../components/Profile'
 import { Projects } from '../../components/Projects'
@@ -6,10 +6,12 @@ import { Posts } from '../../components/Posts'
 
 export function Home() {
     return (
-        <Profile />
-        // <aside id="activity">
-        //     <Projects reposToDisplay={2}/>
-        //     <Posts postsToDisplay={5}/>
-        // </aside>
+        <Container>
+            <Profile />
+            <aside id="activity">
+                <Projects reposToDisplay={2}/>
+                <Posts postsToDisplay={5}/>
+            </aside>
+        </Container>
     )
 }
---
+++ src/pages/Home/styles.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    main {
+        margin-bottom: 30px;
+    }
+`
