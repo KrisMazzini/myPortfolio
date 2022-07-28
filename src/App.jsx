65122624bdada91da52d6fe85ed1fd1e7e632ea7
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-27 23:42:48 -0300

    add projects path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home'
+import { AllProjects } from './pages/Projects'
 
 export function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />}></Route>
+                <Route path='/projects' element={<AllProjects />}></Route>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,7 @@
+import { Projects } from "../../components/Projects";
+
+export function AllProjects() {
+    return (
+        <Projects />
+    )
+}
