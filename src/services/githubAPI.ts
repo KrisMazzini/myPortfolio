fa02575f8e96b09848243d8ce9257623b044e13d
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-30 17:33:37 -0300

    adjust github apis

--- src/services/githubAPI.ts
+++ src/services/githubAPI.ts
@@ -1,3 +1,5 @@
-export const profileURL:string = "https://api.github.com/users/KrisMazzini/"
-export const reposURL:string = "https://api.github.com/users/KrisMazzini/repos";
-export const eventsURL:string = "https://api.github.com/users/KrisMazzini/events"
+const user = `KrisMazzini`
+
+export const profileURL:string = `https://api.github.com/users/${user}/`
+export const reposURL:string = `https://api.github.com/users/${user}/repos`;
+export const eventsURL:string = `https://api.github.com/users/${user}/events`
